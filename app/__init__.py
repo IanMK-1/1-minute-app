3d03a2c54563af0e3a521ce61cbe9f0f0241b2ab
ian mwangi <iankimotho233@gmail.com>
2020-02-09 12:15:54 +0300

    Add the shell context function to allow communication to database

--- app/__init__.py
+++ app/__init__.py
@@ -11,8 +11,10 @@ def create_app(config_name):
     app = Flask(__name__)
 
     # creating the app configurations
-    app = app.config.from_object(config_options[config_name])
+    app.config.from_object(config_options[config_name])
 
     # initializing the flask extensions
     bootstrap.init_app(app)
     db.init_app(app)
+
+    return app
