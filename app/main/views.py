926a858bc1f2af0afb98911901f0676a1e7c288f
ian mwangi <iankimotho233@gmail.com>
2020-02-09 14:53:44 +0300

    Create an email column and a view for login page

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,14 @@
 from flask import render_template
 from . import main
+from flask_login import login_required
 
 
 @main.route('/')
 def index():
     return render_template('index.html')
+
+
+@main.route('/pitch/new-pitch')
+@login_required
+def new_pitch():
+    return render_template('pitch.html')
--- app/models.py
+++ app/models.py
@@ -1,12 +1,15 @@
 from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from . import login_manager
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     __tablename__ = 'pitch_users'
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(255))
+    username = db.Column(db.String(255), index=True)
+    email = db.Column(db.String(255), unique=True, index=True)
     user_right_id = db.Column(db.Integer, db.ForeignKey('rights.id'))
     user_password = db.Column(db.String(255))
 
@@ -25,6 +28,9 @@ class User(db.Model):
     def verify_password(self, password):
         return check_password_hash(self.user_password, password)
 
+    @login_manager.user_loader
+    def load_user(pitch_user_id):
+        return User.query.get(int(pitch_user_id))
 
 class UserRights(db.Model):
     __tablename__ = 'rights'
