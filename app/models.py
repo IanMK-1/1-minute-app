fba86e4ab4140f315e13b91be936b7c4ade05c32
ian mwangi <iankimotho233@gmail.com>
2020-02-09 12:39:10 +0300

    Create a user rights class that allow to set the right of user

--- app/models.py
+++ app/models.py
@@ -6,6 +6,20 @@ class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(255))
+    user_right_id = db.Column(db.Integer, db.ForeignKey('rights.id'))
 
+    # return a printable representation of the object
     def __repr__(self):
         return f'User{self.username}'
+
+
+class UserRights(db.Model):
+    __tablename__ = 'rights'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_rights = db.Column(db.String(255))
+    users_rights = db.relationship('User', backref='right', lazy="dynamic")
+
+    # return a printable representation of the object
+    def __repr__(self):
+        return f'User {self.user_rights}'
