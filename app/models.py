2b956dd6d1f6c55ef1350632dcac443eab7424bf
ian mwangi <iankimotho233@gmail.com>
2020-02-09 14:11:22 +0300

    Create a password column and method to verify password

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class User(db.Model):
@@ -7,10 +8,22 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(255))
     user_right_id = db.Column(db.Integer, db.ForeignKey('rights.id'))
+    user_password = db.Column(db.String(255))
 
     # return a printable representation of the object
     def __repr__(self):
         return f'User{self.username}'
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.user_password = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.pass_secure, password)
 
 
 class UserRights(db.Model):
